3825f53aa8bef12d39ae22b5f93cb50f4c2d3673
Nikolas Santis <nikosantis@gmail.com>
2020-02-25 09:44:13 -0300

    async / await

--- src/es8/index.js
+++ src/es8/index.js
@@ -32,3 +32,33 @@ console.log('food'.padEnd(12, '  -----'))
 const obj = {
   name: 'Nikolas',
 }
+
+//
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+      (true)
+        ? setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Error'))
+  })
+}
+
+const helloAsync = async () => {
+  console.log('Pre Hello')
+  const hello = await helloWorld()
+  console.log(hello)
+}
+
+helloAsync()
+
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld()
+    console.log(hello)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+anotherFunction()
